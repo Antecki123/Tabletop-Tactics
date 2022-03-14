3d6a84e67f93770900a2359372528d380fc15fc2
Antecki123 <mate.antkowiak@gmail.com>
2022-03-14 22:14:19 +0100

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField, Range(1, 30)] private float panSpeed = 20f;
+    [SerializeField, Range(1, 30)] private float scrollSpeed = 20f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 position = transform.position;
+
+        if (Input.GetKey("w"))
+            position.z -= panSpeed * Time.deltaTime;
+        if (Input.GetKey("s"))
+            position.z += panSpeed * Time.deltaTime;
+        if (Input.GetKey("a"))
+            position.x += panSpeed * Time.deltaTime;
+        if (Input.GetKey("d"))
+            position.x -= panSpeed * Time.deltaTime;
+
+        position.y -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * 100f * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, -5f, 5f);
+        position.y = Mathf.Clamp(position.y, 3f, 10f);
+        position.z = Mathf.Clamp(position.z, -8f, 8f);
+        transform.position = position;
     }
 }
