9d616c618eb7ab4c4e5eee0f652d0007075aa6e6
Antecki123 <mate.antkowiak@gmail.com>
2022-03-24 01:27:16 +0100

    Update RollToWound.cs

--- Assets/Scripts/RollToWound.cs
+++ Assets/Scripts/RollToWound.cs
@@ -2,10 +2,10 @@ public static class RollToWound
 {
     private static int[,] result =
     {
-        { 4, 5, 5, 6, 6, 7, 7, 7, 0, 0 },
-        { 4, 4, 5, 5, 6, 6, 7, 7, 7, 0 },
-        { 3, 4, 4, 5, 5, 6, 6, 7, 7, 7 },
-        { 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 },
+        { 4, 5, 5, 6, 6, 7, 8, 9, 0, 0 },
+        { 4, 4, 5, 5, 6, 6, 7, 8, 9, 0 },
+        { 3, 4, 4, 5, 5, 6, 6, 7, 8, 9 },
+        { 3, 3, 4, 4, 5, 5, 6, 6, 7, 8 },
         { 3, 3, 3, 4, 4, 5, 5, 6, 6, 7 },
         { 3, 3, 3, 3, 4, 4, 5, 5, 6, 6 },
         { 3, 3, 3, 3, 3, 4, 4, 5, 5, 6 },
@@ -18,12 +18,36 @@ public static class RollToWound
     {
         var requiredResult = result[defence - 1, strength - 1];
         var rollResult = RollTest.RollDiceD6();
+        var secondRollResult = RollTest.RollDiceD6();
 
         RollResultsPanel.instance.ShowResult(rollResult, "Roll to wound");
 
-        if (requiredResult <= 6 && requiredResult <= rollResult)
-            return true;
+        if (requiredResult <= 6)
+        {
+            if (requiredResult <= rollResult)
+                return true;
+        }
 
-        else return false;
+        else
+        {
+            RollResultsPanel.instance.ShowResult(secondRollResult, "Roll to wound");
+
+            if (requiredResult == 7 && rollResult == 6 && secondRollResult >= 4)
+                return true;
+
+            else if (requiredResult == 8 && rollResult == 6 && secondRollResult >= 5)
+                return true;
+
+            else if (requiredResult == 9 && rollResult == 6 && secondRollResult >= 6)
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool IsPossibleToAttack(int defence, int strength)
+    {
+        var possibility = (result[defence - 1, strength - 1] != 0) ? true : false;
+        return possibility;
     }
 }
