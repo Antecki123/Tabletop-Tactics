0200edf58b720de22e56c9789a55568dfee0ae0b
Antecki123 <mate.antkowiak@gmail.com>
2022-04-01 23:42:06 +0200

    Create Support.cs

---
+++ Assets/Scripts/Support.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Support
+{
+    public void UpdateAction()
+    {
+        Debug.Log("Support");
+    }
+}
