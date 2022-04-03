aadab2594cea2933e555b67d3eabd37a09a8763d
Antecki123 <mate.antkowiak@gmail.com>
2022-04-03 13:38:22 +0200

    Create CombatWeapon.cs

---
+++ Assets/Scripts/CombatWeapon.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Wargear", menuName = "Scriptable Objects/New Combat Weapon")]
+public class CombatWeapon : ScriptableObject
+{
+    public enum WeaponType { None, Sword, WarSpear, ThrowSpear, Pike, StuffOfPower, Whip, Mace, Knife, Club }
+
+    public WeaponType type;
+    public int strength;
+}
