14472561483e4182c13be8c970841cd5f5e0eba6
Antecki123 <mate.antkowiak@gmail.com>
2022-05-04 17:30:31 +0200

    Update GridNode.cs

--- Assets/Scripts/GridNode.cs
+++ Assets/Scripts/GridNode.cs
@@ -14,6 +14,43 @@ public class GridNode : MonoBehaviour
     public Vector2Int Position { get => position; set => position = value; }
     public int MovementValue { get => movementValue; set => movementValue = value; }
 
+    public void HighlightNode(Color color)
+    {
+        var nodeLine = GetComponent<LineRenderer>();
+        nodeLine.enabled = true;
+        nodeLine.startColor = color;
+        nodeLine.endColor = color;
+
+        nodeLine.startWidth = .1f;
+        nodeLine.endWidth = .1f;
+
+        for (int i = 0; i < nodeLine.positionCount; i++)
+        {
+            var newPosition = new Vector3(nodeLine.GetPosition(i).x, .1f, nodeLine.GetPosition(i).z);
+            nodeLine.SetPosition(i, newPosition);
+        }
+    }
+
+    public void ClearHighlight()
+    {
+        var nodeLine = GetComponent<LineRenderer>();
+
+        nodeLine.enabled = true;
+        nodeLine.startColor = Color.white;
+        nodeLine.endColor = Color.white;
+
+        nodeLine.startWidth = .02f;
+        nodeLine.endWidth = .02f;
+
+        for (int i = 0; i < nodeLine.positionCount; i++)
+        {
+            var newPosition = new Vector3(nodeLine.GetPosition(i).x, 0f, nodeLine.GetPosition(i).z);
+            nodeLine.SetPosition(i, newPosition);
+        }
+
+        MovementValue = 0;
+    }
+
     /*
     private void OnMouseOver()
     {
